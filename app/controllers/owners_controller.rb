e0d9a339b6df8a6accbf081b082a4b2fd80a6b08
Burak Kocak <burakkocak884@ghotmail.com>
2019-04-09 17:26:54 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class OwnersController < ApplicationController
 
   get '/owners' do
@@ -6,24 +7,54 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+
+    @pets = Pet.all
+
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+  
+  #binding.pry
+  @owner = Owner.create(params[:owner])
+  
+
+  if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
   end
+  #binding.pry
+  @owner.save
+  redirect "/owners/#{@owner.id}"
+end
 
   get '/owners/:id/edit' do 
+    #binding.pry
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
 
   get '/owners/:id' do 
     @owner = Owner.find(params[:id])
+    #binding.pry
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    #######
+ 
+    @owner = Owner.find_by(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 end
+
+
+
+
